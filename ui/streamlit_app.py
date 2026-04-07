2ee0dbec5456d94f75711d2cc779d48a4e3e41fc
joel-varghese <joelvar17@outlook.com>
2026-04-07 10:35:53 -0400

    customizing

--- ui/streamlit_app.py
+++ ui/streamlit_app.py
@@ -11,6 +11,11 @@ st.set_page_config(page_title="AI Travel Concierge", layout="centered")
 
 st.title("✈️ AI Travel Concierge Demo")
 
+if "show_recs" not in st.session_state:
+    st.session_state.show_recs = False
+if "data" not in st.session_state:
+    st.session_state.data = None
+
 member_id = st.text_input("Enter Member ID", "123")
 
 if st.button("Get Recommendations"):
@@ -21,22 +26,38 @@ if st.button("Get Recommendations"):
         rules = get_partner_config(member["partner_id"])
         recs = generate_recommendations(member, rules)
 
-    if member and rules and recs:
+        st.session_state.data = {
+            "member": member,
+            "rules": rules,
+            "recs": recs
+        }
 
-        st.subheader("👤 Member Info")
-        st.json(member)
+        st.session_state.show_recs = True
 
-        st.subheader("⚙️ Applied Rules")
-        st.json(rules)
+if st.session_state.data:
+    toggle_label = "❌ Hide Recommendations" if st.session_state.show_recs else "✅ Show Recommendations"
 
-        st.subheader("🌍 Recommendations")
+    if st.button(toggle_label):
+        st.session_state.show_recs = not st.session_state.show_recs
 
-        for rec in recs:
-            with st.container():
-                st.markdown(f"### {rec['destination']}")
-                st.write(f"Type: {rec['type']}")
-                st.write(f"Score: {rec['score']}")
-                st.divider()
+if st.session_state.show_recs and st.session_state.data:
 
-    else:
-        st.error("Something went wrong")
+    member = get_member(member_id)
+    rules = get_partner_config(member["partner_id"])
+    recs = generate_recommendations(member, rules)
+
+
+    st.subheader("👤 Member Info")
+    st.json(member)
+
+    st.subheader("⚙️ Applied Rules")
+    st.json(rules)
+
+    st.subheader("🌍 Recommendations")
+
+    for rec in recs:
+        with st.container():
+            st.markdown(f"### {rec['destination']}")
+            st.write(f"Type: {rec['type']}")
+            st.write(f"Score: {rec['score']}")
+            st.divider()
